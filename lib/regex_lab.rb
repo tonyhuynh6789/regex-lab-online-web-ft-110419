a974e1aea86d6be44e02a21a4db271971634f5c8
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-11 22:08:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,18 +16,10 @@ return_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
-    true
-  else  
-    false
-  end
+
+  
 end
 
 def valid_phone_number?(phone)
-if text.match(/^[A-Z]/) && text.match(/[\.]$/)
-    true
-  else 
-    false
-  end
+i=
 end
