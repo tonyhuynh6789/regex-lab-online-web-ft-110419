99f6ae9a60bd1964b0044657c44f52b289e017ac
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-11 22:08:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,11 @@ return_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else 
+    false
+  end
   
 end
 
