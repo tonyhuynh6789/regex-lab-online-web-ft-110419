865c150b18f26023dbf72403ea36850916c8bc85
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-11 22:07:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,12 @@ return_array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+first_letter = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    true
+  else  
+    false
+  end
 end
 
 def valid_phone_number?(phone)
